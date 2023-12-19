69e0f15134c379261d4bbcb2413d454aa683e75d
Crizza <ivan.alineri@outlook.com>
2023-12-19 18:11:17 -0300

    melhorias
    - Criação de duas classes que ficam responsáveis por gerenciar as informações da tree

--- web_scrap/main.py
+++ web_scrap/main.py
@@ -11,72 +11,42 @@ orixa_element = ''
 orixa_dominance = ''
 orixa_salute = ''
 
+
+def extract_text(tree_html, xpath):
+    data = tree_html.xpath(xpath)
+    return data[0].text_content().strip() if data else None
+
+
+def extract_attribute(tree_html, xpath, attribute):
+    data = tree_html.xpath(xpath + '/@' + attribute)
+    return data[0] if data else None
+
+
 url = 'https://ocandomble.com/os-orixas/nana/'
 
 response = requests.get(url)
 webpage = response.content
 
 tree = html.fromstring(webpage)
-
-xpath_day = '//*[@id="post-20"]/div[2]/p[3]'
-xpath_color = '//*[@id="post-20"]/div[2]/p[4]'
-xpath_symbols = '//*[@id="post-20"]/div[2]/p[5]'
-xpath_element = '//*[@id="post-20"]/div[2]/p[6]'
-xpath_dominance = '//*[@id="post-20"]/div[2]/p[7]'
-xpath_salute = '//*[@id="post-20"]/div[2]/p[8]'
-xpath_salutes = '//*[@id="post-20"]/div[2]/p[8]'
-xpath_title = '//*[@id="post-20"]/div[1]/h2/a'
-xpath_img = '//*[@id="post-20"]/div[2]/p[2]/strong/span/a/img'
-
-data = tree.xpath(xpath_title)
-for title in data:
-    orixa_tit = title.text_content()
-    print(orixa_tit)
-
-data = tree.xpath(xpath_img + '/@src')
-for img in data:
-    orixa_img = img
-    print(orixa_img)
-
-data = tree.xpath(xpath_day)
-for dia in data:
-    orixa_dia = dia.text_content()
-    print(orixa_dia)
-
-data = tree.xpath(xpath_color)
-for color in data:
-    orixa_cor = color.text_content()
-    print(orixa_cor)
-
-data = tree.xpath(xpath_symbols)
-for symbols in data:
-    orixa_symbols = symbols.text_content()
-    print(orixa_symbols)
-
-data = tree.xpath(xpath_element)
-for element in data:
-    orixa_element = element.text_content()
-    print(orixa_element)
-
-data = tree.xpath(xpath_dominance)
-for dominance in data:
-    orixa_dominance = dominance.text_content()
-    print(orixa_dominance)
-
-data = tree.xpath(xpath_salutes)
-for salute in data:
-    orixa_salute = salute.text_content()
-    print(orixa_salute)
-
+xpaths = {
+    "title": '//*[@id="post-20"]/div[1]/h2/a',
+    "img": '//*[@id="post-20"]/div[2]/p[2]/strong/span/a/img',
+    "day": '//*[@id="post-20"]/div[2]/p[3]',
+    "color": '//*[@id="post-20"]/div[2]/p[4]',
+    "symbols": '//*[@id="post-20"]/div[2]/p[5]',
+    "element": '//*[@id="post-20"]/div[2]/p[6]',
+    "dominance": '//*[@id="post-20"]/div[2]/p[7]',
+    "salutes": '//*[@id="post-20"]/div[2]/p[8]'
+}
 orixa_info = {
-    "título": orixa_tit.strip(),
-    "imagem": orixa_img.strip(),
-    "dia": orixa_dia.strip(),
-    "cor": orixa_cor.strip(),
-    "símbolos": orixa_symbols.strip(),
-    "elemento": orixa_element.strip(),
-    "domínio": orixa_dominance.strip(),
-    "saudações": orixa_salute.strip()
+    "título": extract_text(tree, xpaths["title"]),
+    "imagem": extract_attribute(tree, xpaths["img"], "src"),
+    "dia": extract_text(tree, xpaths["day"]),
+    "cor": extract_text(tree, xpaths["color"]),
+    "símbolos": extract_text(tree, xpaths["symbols"]),
+    "elemento": extract_text(tree, xpaths["element"]),
+    "domínio": extract_text(tree, xpaths["dominance"]),
+    "saudações": extract_text(tree, xpaths["salutes"])
 }
 
 with open('orixa_info.json', 'w', encoding='utf-8') as f:

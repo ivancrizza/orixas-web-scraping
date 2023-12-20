eac489bbb4a9085b55e98358fb33af3d92bd5b9c
Crizza <ivan.alineri@outlook.com>
2023-12-20 12:03:49 -0300

    orixas
    json completo

--- web_scrap/main.py
+++ web_scrap/main.py
@@ -22,21 +22,21 @@ def extract_attribute(tree_html, xpath, attribute):
     return data[0] if data else None
 
 
-url = 'https://ocandomble.com/os-orixas/iroko/'
+url = 'https://ocandomble.com/os-orixas/oba/'
 
 response = requests.get(url)
 webpage = response.content
 
 tree = html.fromstring(webpage)
 xpaths = {
-    "title": '//*[@id="post-263"]/div[1]/h2/a',
-    "img": '//*[@id="post-263"]/div[2]/p[1]/strong/a/img',
-    "day": '//*[@id="post-263"]/div[2]/p[2]',
-    "color": '//*[@id="post-263"]/div[2]/p[3]',
-    "symbols": '//*[@id="post-263"]/div[2]/p[4]',
-    "element": '//*[@id="post-263"]/div[2]/p[4]',
-    "dominance": '//*[@id="post-263"]/div[2]/p[6]',
-    "salutes": '//*[@id="post-263"]/div[2]/p[7]'
+    "title": '//*[@id="post-135"]/div[1]/h2/a',
+    "img": '//*[@id="post-135"]/div[2]/p[1]/strong/a/img',
+    "day": '//*[@id="post-20"]/div[2]/p[3]',
+    "color": '//*[@id="post-20"]/div[2]/p[4]',
+    "symbols": '//*[@id="post-20"]/div[2]/p[5]',
+    "element": '//*[@id="post-20"]/div[2]/p[6]',
+    "dominance": '//*[@id="post-20"]/div[2]/p[7]',
+    "salutes": '//*[@id="post-20"]/div[2]/p[8]'
 }
 
 # IANSA PRECISA FAZER NA MAO
@@ -50,7 +50,7 @@ xpaths = {
 #     "dominance": '//*[@id="post-20"]/div[2]/p[7]',
 #     "salutes": '//*[@id="post-20"]/div[2]/p[8]'
 # }
-orixa_info_iroko = {
+orixa_info_nana = {
     "título": extract_text(tree, xpaths["title"]),
     "imagem": extract_attribute(tree, xpaths["img"], "src"),
     "dia": extract_text(tree, xpaths["day"]),
@@ -60,14 +60,14 @@ orixa_info_iroko = {
     "domínio": extract_text(tree, xpaths["dominance"]),
     "saudações": extract_text(tree, xpaths["salutes"])
 }
-# print(extract_text(tree, xpaths["title"]))
-# print(extract_attribute(tree, xpaths["img"], "src"))
-# print(extract_text(tree, xpaths["day"]))
-# print(extract_text(tree, xpaths["color"]))
-# print(extract_text(tree, xpaths["symbols"]))
-# print(extract_text(tree, xpaths["element"]))
-# print(extract_text(tree, xpaths["dominance"]))
-# print(extract_text(tree, xpaths["salutes"]))
+print(extract_text(tree, xpaths["title"]))
+print(extract_attribute(tree, xpaths["img"], "src"))
+print(extract_text(tree, xpaths["day"]))
+print(extract_text(tree, xpaths["color"]))
+print(extract_text(tree, xpaths["symbols"]))
+print(extract_text(tree, xpaths["element"]))
+print(extract_text(tree, xpaths["dominance"]))
+print(extract_text(tree, xpaths["salutes"]))
 
-with open('orixa_info_iroko.json', 'w', encoding='utf-8') as f:
-    json.dump(orixa_info_iroko, f, ensure_ascii=False, indent=4)
+# with open('orixa_info_nana.json', 'w', encoding='utf-8') as f:
+#     json.dump(orixa_info_nana, f, ensure_ascii=False, indent=4)

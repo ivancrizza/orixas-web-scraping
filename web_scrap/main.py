18a4ce5e4042451cb49d16af5b1d65b9a1a7592a
Crizza <ivan.alineri@outlook.com>
2023-12-20 09:26:25 -0300

    oxum

--- web_scrap/main.py
+++ web_scrap/main.py
@@ -22,22 +22,24 @@ def extract_attribute(tree_html, xpath, attribute):
     return data[0] if data else None
 
 
-url = 'https://ocandomble.com/os-orixas/yemonja/'
+url = 'https://ocandomble.com/os-orixas/oxum/'
 
 response = requests.get(url)
 webpage = response.content
 
 tree = html.fromstring(webpage)
 xpaths = {
-    "title": '//*[@id="post-72"]/div[1]/h2/a',
-    "img": '//*[@id="post-72"]/div[2]/p[1]/strong/span/a/img',
-    "day": '//*[@id="post-72"]/div[2]/p[2]',
-    "color": '//*[@id="post-72"]/div[2]/p[3]',
-    "symbols": '//*[@id="post-72"]/div[2]/p[4]',
-    "element": '//*[@id="post-72"]/div[2]/p[5]',
-    "dominance": '//*[@id="post-72"]/div[2]/p[6]',
-    "salutes": '//*[@id="post-72"]/div[2]/p[7]'
+    "title": '//*[@id="post-76"]/div[1]/h2/a',
+    "img": '//*[@id="post-76"]/div[2]/p[1]/img',
+    "day": '//*[@id="post-76"]/div[2]/p[2]',
+    "color": '//*[@id="post-76"]/div[2]/p[3]',
+    "symbols": '//*[@id="post-76"]/div[2]/p[4]',
+    "element": '//*[@id="post-76"]/div[2]/p[5]',
+    "dominance": '//*[@id="post-76"]/div[2]/p[6]',
+    "salutes": '//*[@id="post-76"]/div[2]/p[7]'
 }
+
+# IANSA PRECISA FAZER NA MAO
 # nana_xpaths = {
 #     "title": '//*[@id="post-20"]/div[1]/h2/a',
 #     "img": '//*[@id="post-20"]/div[2]/p[2]/strong/span/a/img',
@@ -48,7 +50,7 @@ xpaths = {
 #     "dominance": '//*[@id="post-20"]/div[2]/p[7]',
 #     "salutes": '//*[@id="post-20"]/div[2]/p[8]'
 # }
-orixa_info_yemanja = {
+orixa_info_oxum = {
     "título": extract_text(tree, xpaths["title"]),
     "imagem": extract_attribute(tree, xpaths["img"], "src"),
     "dia": extract_text(tree, xpaths["day"]),
@@ -67,5 +69,5 @@ orixa_info_yemanja = {
 # print(extract_text(tree, xpaths["dominance"]))
 # print(extract_text(tree, xpaths["salutes"]))
 
-with open('orixa_info_yemanja.json', 'w', encoding='utf-8') as f:
-    json.dump(orixa_info_yemanja, f, ensure_ascii=False, indent=4)
+with open('orixa_info_oxum.json', 'w', encoding='utf-8') as f:
+    json.dump(orixa_info_oxum, f, ensure_ascii=False, indent=4)
